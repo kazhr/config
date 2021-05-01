bacd9f7397bb80df25ff8cbdcc221ef6239ed26f
kazhr <74481424+kazhr@users.noreply.github.com>
2021-05-01 18:11:17 +0900

    [modify] use class

--- ansible/testinfra/apache.py
+++ ansible/testinfra/apache.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def test_apache_is_installed(host):
-    apache = host.package("httpd")
-    assert apache.is_installed
+class TestApache:
+
+    def test_apache_is_installed(self, host):
+        package = host.package("httpd")
+        assert package.is_installed
+
+    def test_apache_is_running(self, host):
+        service = host.service("httpd")
+        assert service.is_running
+        assert service.is_enabled
