6924adb303d85bf7867bf5419fb2b6edb336babd
kazhr <74481424+kazhr@users.noreply.github.com>
2021-05-04 13:29:47 +0900

    [update] test_apache

--- ansible/testinfra/tests/test_apache.py
+++ ansible/testinfra/tests/test_apache.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import requests
+
 
 class TestApache:
 
@@ -11,3 +14,16 @@ class TestApache:
         service = host.service("httpd")
         assert service.is_running
         assert service.is_enabled
+
+    def test_apache_test_page(self, host):
+        hostname = host.backend.hostname
+        ansible_variables = host.backend.ansible_runner.get_variables(hostname)
+        ansible_host = ansible_variables["ansible_host"]
+        url = f"http://{ansible_host}"
+
+        res = requests.get(url)
+        assert res.status_code == 403
+
+        soup = BeautifulSoup(res.text, "html.parser")
+        title = soup.find("title").text
+        assert title == "Apache HTTP Server Test Page powered by CentOS"
