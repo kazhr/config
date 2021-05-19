af6de4b8d831fbe841fa9bf363ce06a35481984e
kazhr <74481424+kazhr@users.noreply.github.com>
2021-05-19 16:35:22 +0900

    [add] flask

---
+++ python/flask/hello_world/hello_world.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    msg = "Hello, World!"
+    return msg
+
+
+@app.route("/<name>")
+def hello(name):
+    return render_template("hello.html", name=name)
+
+
+if __name__ == "__main__":
+    app.run(
+        host="127.0.0.1",
+        port=8888,
+        debug=True,
+    )
