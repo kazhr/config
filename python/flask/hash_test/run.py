98dd3d7d6bbaa0ae10df96a3a88e2c05b7857a3d
kazhr <74481424+kazhr@users.noreply.github.com>
2021-05-20 10:41:43 +0900

    [update] use form & validators

--- python/flask/hash_test/run.py
+++ python/flask/hash_test/run.py
@@ -1,22 +1,48 @@
 from flask import flash, Flask, request, render_template, redirect, url_for
+from flask_wtf import FlaskForm
 from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import PasswordField, validators
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "change me!"
 
 
+class PasswordForm(FlaskForm):
+    """
+    パスワード入力用のForm
+    """
+    password = PasswordField(
+        label="password",
+        validators=[
+            validators.DataRequired(),
+            validators.Length(min=8, max=32, message='8文字以上32文字以内で入力!'),
+            validators.Regexp('(?=.*?[A-Z])', message="一文字以上の大文字を含めてください!"),
+            validators.Regexp('(?=.*?[0-9])', message="一文字以上の数字を含めてください!"),
+            validators.Regexp('(?=.*?[#?!@$%^&*-])', message="一文字以上の記号を含めてください!(使用できる記号: #?!@$%^&*-)"),
+        ],
+    )
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "GET":
-        return render_template("input.html")
+    """
+    パスワード入力
+    """
+    form = PasswordForm(request.form)
+    if form.validate_on_submit():
 
-    password = request.form["password"]
-    hashed_pass = generate_password_hash(password)
-    return redirect(url_for("check", hashed_pass=hashed_pass))
+        password = form.password.data
+        hashed_pass = generate_password_hash(password)
+        return redirect(url_for("check", hashed_pass=hashed_pass))
+
+    return render_template("input.html", form=form)
 
 
 @app.route("/<hashed_pass>", methods=["GET", "POST"])
 def check(hashed_pass):
+    """
+    ハッシュ化されたパスワードと一致するか
+    """
     if request.method == "GET":
         return render_template("check.html", hashed_pass=hashed_pass)
 
