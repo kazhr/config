d5648b0def8efa5af08d6387e01632d63d94354e
kazhr <74481424+kazhr@users.noreply.github.com>
2021-06-04 10:05:06 +0900

    [add] jinja macro test

--- python/jinja/show_template.py
+++ python/jinja/show_template.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from jinja2 import Environment, FileSystemLoader
+import sys
 
 env = Environment(
     loader=FileSystemLoader("."),
     lstrip_blocks=True
 )
 
-template = env.get_template("./template.txt.j2")
+template = env.get_template(sys.argv[1])
 rendered = template.render()
 
 print(rendered)
