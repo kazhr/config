3d299bea420d8059c60b7277556117ea0dcc55a4
kazhr <74481424+kazhr@users.noreply.github.com>
2021-06-14 21:42:32 +0900

    [add] flask api_test

--- python/flask/api_test/api.py
+++ python/flask/api_test/api.py
@@ -3,9 +3,14 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/api/")
 def response():
     data = {
         "key": "value"
     }
     return jsonify(data)
+
+
+@app.route("/api/health")
+def healthcheck():
+    return "ok", 200
