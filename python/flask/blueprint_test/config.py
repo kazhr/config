b6f4bc61e20aed14f0e4bd224dac4605790e0633
kazhr <74481424+kazhr@users.noreply.github.com>
2022-07-16 22:46:27 +0900

    [rm] python: moved to the sandbox repository

--- python/flask/blueprint_test/config.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-class Config:
-    SECRET_KEY = "secret"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
--- python/flask/db_test/config.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-class Config:
-    SECRET_KEY = "secret"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
